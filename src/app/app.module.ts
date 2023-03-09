5ed61c92aa1aa12229d6da36a70530545c7b1ce1
NaderAyed <nader.ayed@esprit.tn>
2023-03-09 19:40:11 +0100

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListEditComponent } from './shoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipeBook/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from "./recipeBook/recipe-detail/recipe-detail.component";
+import { HeaderComponent } from "./header/header.component";
+import { ShoppingListComponent } from './shoppingList/shopping-list/shopping-list.component'
+import { RecipesComponent } from "./recipeBook/recipes.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
